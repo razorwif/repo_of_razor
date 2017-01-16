5e0d0bee04e0e51daedf3718afd3a2927c851474
razorwif <hilumpia@gmail.com>
2017-01-16 15:42:05 +0100

    Add files via upload
    ajout du CSS et des fichier liés.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Mon super site</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+
+		<form action="cible.php" method="POST">
+		<p><label>Prenom : <input type="text" name="Prenom"></label></p>
+		<p><input type="submit" /></p>
+
+
+		</form>
+
+	</body>
+</html>
